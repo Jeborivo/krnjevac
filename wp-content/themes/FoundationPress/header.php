232bce7ba8cd20ee69c77d87992170e130b18f67
bora <borarooney@gmail.com>
2018-12-14 16:55:55 +0100

    hero,header and menu almost done

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -43,10 +43,60 @@
 			</div>
 			<div class="top-bar-right">
 				<?php foundationpress_top_bar_r(); ?>
-
+				<div class="search"></div>
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
+				<div class="wrap-search">
+					<input id="open" class="search-toggle" type="checkbox">
+					<label for="open" class="label-toggle">Search</label>
+					<div class="search-content">
+						<input class="search-content-inner" type="text">
+					</div>
+				</div>
+
+				<a href="#" class="shop">shop</a>
+				<div class="navigation"> 	
+            	<input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+           		<label for="navi-toggle" class="navigation__button">
+                	<span class="navigation__icon">&nbsp;</span>
+				</label>
+				
+            	<div class="navigation__background">
+					&nbsp;
+				</div>
+		
+            	<nav class="navigation__nav">
+				<?php foundationpress_top_bar_l(); ?>
+				<div class="info">
+					<h3 class="info_title">Medino DOO Krnjevo</h3>
+					<h5 class="info_phone">+381 26 821 080</h5>
+					<h5 class="info_mail">office@krnjevac.rs</h5>
+					<h5 class="info_adress">Bulevar oslobođenja 29, 11319 Krnjevo, Srbija</h5>
+				</div>
+				<div class="nav_links">
+					<div class="faq">
+						<a href="#" class="faq_link">Kontrola kvaliteta</a>
+						<a href="#" class="faq_link">Gde kupiti</a>
+						<a href="#" class="faq_link">Uslovi korišćenja i prodaje</a>
+						<a href="#" class="faq_link">Kako poručiti proizvod</a>
+					</div>
+					<div class="about">
+						<a href="#" class=about_link">Blog</a>
+						<a href="#" class=about_link">Naši pčelari</a>
+						<a href="#" class=about_link">Projekti</a>
+						<a href="#" class=about_link">Recepti</a>
+					</div>
+					<div class="social">
+						<a href="#" class=social_link">Facebook</a>		
+						<a href="#" class=social_link">Instagram</a>	
+						<a href="#" class=social_link">Youtube</a>	
+					</div>
+				</div>
+            	</nav>
+        	</div>
+
+
 			</div>
 		</nav>
 
