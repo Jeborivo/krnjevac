fa8aa6b8069400d0beef48b2e47a7ebe44d85cac
bora <borarooney@gmail.com>
2018-12-14 12:38:45 +0100

    foundationPress

---
+++ wp-content/themes/FoundationPress/header.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "container" div.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?> >
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+
+	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
+	<?php endif; ?>
+
+
+	<header class="site-header" role="banner">
+		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
+			<div class="title-bar-left">
+				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+				<span class="site-mobile-title title-bar-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				</span>
+			</div>
+		</div>
+
+		<nav class="site-navigation top-bar" role="navigation">
+			<div class="top-bar-left">
+				<div class="site-desktop-title top-bar-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				</div>
+				<?php foundationpress_top_bar_l(); ?>
+			</div>
+			<div class="top-bar-right">
+				<?php foundationpress_top_bar_r(); ?>
+
+				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+				<?php endif; ?>
+			</div>
+		</nav>
+
+		<div class="hero">
+			<div class="hero_content main-container">
+				<div class="hero_content--bg-video">
+					<video class="bg-video_content" autoplay muted loop>
+						<source src="wp-content/themes/FoundationPress/src/assets/images/Honey.webm" type="video/webm">
+						<source src="wp-content/themes/FoundationPress/src/assets/images/Honey.mp4" type="video/mp4">
+					</video>
+				</div>
+
+				<div class="hero_content--text">
+					<h1 class="hero_content--title">Ne mešamo se u posao prirode</h1>
+					<h4 class="hero_content--desc">Posebno i pažljivo odabran med bez dodataka.</h4>
+					<button class="button hero_content--button" type="button">Krnjevac med</button>
+				</div>
+			</div>
+		</div>
+
+
+	</header>
