4d0cd807cb4fa01d0b40d1efb285316fb4d080c9
David <anru1996@gmail.com>
2019-01-11 01:40:22 +0100

    related added from backup

--- wp-content/themes/FoundationPress/woocommerce/single-product.php
+++ wp-content/themes/FoundationPress/woocommerce/single-product.php
@@ -46,7 +46,6 @@ get_header(); ?>
 					<h2><?php echo $product->get_name();?></h2>
 					<h3 class="active_attribute"></h3>
 				</div>
-
 				<div class="variation_select product-info_variation-select">
 					<div id="product_id"><?php echo ($product->get_id()); ?></div>
 					<div class="var-button-wrap">
@@ -62,15 +61,12 @@ get_header(); ?>
 					</div>
 					<div class="stockIcon"></div>
 				</div>
-
 				<div class="product_description product-info_description"><?php echo $product->get_data()['description'];?></div>
 					<div class="product-info_link-wrap">
 						<a class="product-info_link-wrap--link tabela_nutritivnih_vrednosti" href="#">Tabela nutritivnih vrednosti <i class="icon-arrow-right"></i></a>
 						<a class="product-info_link-wrap--link kupi_u_prodavnici" href="#">Kupi u prodavnici <i class="icon-arrow-right"></i></a>
 					</div>
-
 					<div class="product-info_price-quantity-to-cart-wrap">
-
 					<?php foreach ( $variations as $variation ) :?>	
 					<?php $br2++;?>
 						<!-- display prices -->			
@@ -84,37 +80,40 @@ get_header(); ?>
 	
 						<input id='cart_quantity' class="quantity product-info_price-quantity-to-cart-wrap--quantity" value="1" type="number" name="quantity" min="1" max="99">
 						<input type="button" id="variation_add_to_cart" class="button product-info_price-quantity-to-cart-wrap--button" onclick="addToCart()"value="Dodaj u korpu">
-					</div>
-
-			
+					</div>			
 			</div><!-- product-info -->
 			<?php endwhile; ?>
 		</div><!--Product-container -->
-		<div class="related-products cards">
-			<div class="related-filters">
-				<div class="related-filters_buttons">
-					<button type="button" class="button btn-grey related-filters_buttons--last">Poslednje pregledano</button>
-					<button type="button" class="button btn-grey related-filters_buttons--most">Najprodavanije</button>
-				</div>
-				<a href="#" class="related-filters_link">Internet-prodavnica <i class="icon-arrow-right"></i></a>
-			</div>
-			<div class="cards-container">
-			<?php $relatedProducts = wc_get_related_products($product->get_id())?>
-
-				<?php foreach ($relatedProducts as $related):?>
+			<!-- custom related  -->
+			<?php 
+			// query
+				$args = array( 'post_type' => 'related_product', 'posts_per_page' => -1,'meta_key'=> 'product_display','meta_value'	=> $product->get_id());
+				$loop = new WP_Query( $args );
+			?>
+			<h2> Related</h2>
+			<?php while ( $loop->have_posts() ) : $loop->the_post();
+			// values
+				$value = get_field( "product_link" );
+				$productRelated = new WC_Product_Variable($value->post_parent );
+				$variable_product = wc_get_product($value->ID);
+			$thumbnail = get_the_post_thumbnail_url($value->ID);
+			?>
 				<div class="card">
-        			<div class="card-content">
-						<?php $relatedProduct = wc_get_product( $related );?>
-						<h4 class="card-content_description--title"><?php echo $relatedProduct->get_title();?></h4>
-						<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $related ), 'single-post-thumbnail' );?>
-						<div class="card-content_image" style="background-image: url('<?php  echo $image[0]; ?>')"></div>
-						<a href="<?php echo get_permalink($related); ?>"> Jos</a>
+        		<div class="card-content">
+					<h3 class="title"><?php echo get_the_title( $value->post_parent ); ?></h3>
+					<h4 class="gramaza"><?php echo $variable_product->get_attributes()['gramaza']; ?></h4>
+					<div class="cena">	<?php echo $variable_product->get_regular_price(); ?></span>,- 
+                                <?php $sale= $variable_product->get_sale_price(); ?>
+                               <?php if($sale != ''):?>
+                               <span id="sale_price"> <?php echo($sale);?></span>
+                                        <?php echo(',-');?>
+								<?php endif; ?>
 					</div>
+					<img class="card-content_image" src="<?php  echo $thumbnail; ?>" >
+					<a href="?add-to-cart=<?php echo ($value->post_parent ); ?>&variation_id=<?php echo $value->ID?>&attribute_gramaza=<?php echo ( $variable_product->get_attributes()['gramaza']); ?>">+</a>
 				</div>
-				<?php endforeach;?>
-			</div> 
-		</div> <!--related-product-container -->
-
+			</div>
+		<?php endwhile;?>
 		<div class="product-buy-info">
 			<div class="product-buy-info_address info-field">
 				<img src="http://localhost/krnjevac/wp-content/themes/FoundationPress/src/assets/images/phone.svg" alt="phone">
