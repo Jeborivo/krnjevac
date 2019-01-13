92e8557293a007e91d13dcb353b499775260e1bc
David <anru1996@gmail.com>
2019-01-13 16:25:10 +0100

    config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'krnjevac_');
 define('DB_USER', 'krnjevac');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'h3P3axZt5C4');
+define('DB_PASSWORD', 'klaustrofobijaX4');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
